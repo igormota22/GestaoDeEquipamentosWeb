3a45b08c41d9335adac3a1c7e28a3a93edb4580c
Igor Mota <igormello399@gmail.com>
2026-05-13 16:50:26 -0300

    Adicionado cadastro de fabricante

---
+++ GestaoDeEquipamentosWeb.ConsoleApp/Controllers/FabricanteController.cs
@@ -0,0 +1,43 @@
+using GestaoDeEquipamentosWeb.ConsoleApp.Compartilhado;
+using GestaoDeEquipamentosWeb.ConsoleApp.Compartilhado.Arquivos;
+using GestaoDeEquipamentosWeb.ConsoleApp.ModuloFabricante;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestaoDeEquipamentosWeb.ConsoleApp.Controllers
+{
+
+    public class FabricanteController : Controller
+    {
+        private readonly IRepositorio<Fabricante> repositorioFabricante;
+        public FabricanteController()
+        {
+            ContextoJson contexto = new ContextoJson();
+            contexto.Carregar();
+
+            repositorioFabricante = new RepositorioFabricanteEmArquivo(contexto);
+        }
+        // GET: FabricanteController
+        public ActionResult listar()
+        {
+            List<Fabricante> fabricantes = repositorioFabricante.SelecionarTodos();
+            return View(fabricantes);
+        }
+
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(string nome, string email, string telefone)
+        {
+            Fabricante novoFabricante = new Fabricante(nome, email, telefone);
+
+            repositorioFabricante.Cadastrar(novoFabricante);
+
+
+            return RedirectToAction(nameof(listar));
+        }
+    }
+}

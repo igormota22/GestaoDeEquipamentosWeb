4566d4caba4f155b8c246a2a29fcab7c3f780365
Igor Mota <igormello399@gmail.com>
2026-05-13 14:07:12 -0300

    Configurado servidor web

--- GestaoDeEquipamentosWeb.ConsoleApp/Program.cs
+++ GestaoDeEquipamentosWeb.ConsoleApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// ASP.Net Core
+
+//Builder de um servidor web
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+//MVC
+builder.Services.AddControllersWithViews();
+
+//Criaçao
+WebApplication app = builder.Build();
+
+app.UseRouting();
+app.MapDefaultControllerRoute();
+
+//Inicia o loop da aplicação
+app.Run();
